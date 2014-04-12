2cba96c7082951233d414d06a99752afa0802a49
hluk <hluk@email.cz>
2014-04-12 08:31:54 +0200

    Install translation into separate directory

--- utils/windows_copyq_update.sh
+++ utils/windows_copyq_update.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 # Script to update CopyQ installation on Windows.
-src=${1:-"../build-copyq-Qt_4_8_5-Release"}
+build=${1:-"../build-copyq-Qt_4_8_5-Release"}
 dst=${2:-"/c/dev/copyq"}
+src=${3:-$PWD}
 
 set -e
 
 update() {
     set -e
-    for file in "$@"; do
-        cp -uv -- "$file" "$dst/$file"
-    done
+    mkdir -p "${@: -1}"
+    cp -uv -- "$@"
 }
 
-mkdir -p "$dst/"{plugins,themes}
-
-update "README.md" "AUTHORS" "HACKING"
-
-cd shared
-update themes/*.ini
-cd -
-
-cd "$src"
-update copyq.exe copyq.com plugins/*.dll
-cd -
-
-
+update "$src"/{README.md,AUTHORS,HACKING} "$dst"
+update "$src"/shared/themes/*.ini "$dst/themes"
+update "$build"/{copyq.exe,copyq.com} "$dst"
+update "$build"/plugins/*.dll "$dst/plugins"
+update "$build"/src/*.qm "$dst/translations"
