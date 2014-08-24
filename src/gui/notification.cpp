6803504d73e24ec46763db8313230bf2438bc52c
Lukas Holecek <hluk@email.cz>
2014-08-24 07:51:50 +0200

    Show notification in dialog on right or middle click

--- src/gui/notification.cpp
+++ src/gui/notification.cpp
@@ -22,17 +22,69 @@
 #include "common/common.h"
 #include "gui/configurationmanager.h"
 #include "gui/iconfactory.h"
+#include "gui/icons.h"
 
 #include <QApplication>
 #include <QClipboard>
+#include <QDialogButtonBox>
 #include <QGridLayout>
+#include <QIcon>
 #include <QLabel>
 #include <QMap>
 #include <QMouseEvent>
 #include <QPainter>
+#include <QPushButton>
+#include <QScopedPointer>
+#include <QTextEdit>
 #include <QTimer>
 #include <QVBoxLayout>
-#include <QVBoxLayout>
+
+namespace {
+
+void showNotificationInspectDialog(const QString &messageTitle, const QString &message)
+{
+    QScopedPointer<QDialog> dialog(new QDialog);
+    dialog->setObjectName("InspectNotificationDialog");
+    dialog->setWindowTitle( Notification::tr("CopyQ Inspect Notification") );
+
+    ConfigurationManager *cm = ConfigurationManager::instance();
+    cm->registerWindowGeometry( dialog.data() );
+
+    if ( cm->value("always_on_top").toBool() )
+        dialog->setWindowFlags( dialog->windowFlags() ^ Qt::WindowStaysOnTopHint );
+
+    IconFactory *iconFactory = cm->iconFactory();
+    dialog->setWindowIcon( iconFactory->appIcon() );
+
+    QTextEdit *editor = new QTextEdit(dialog.data());
+    editor->setReadOnly(true);
+    editor->setText(
+        "<h3>" + escapeHtml(messageTitle) + "</h3>"
+        "<p>" + escapeHtml(message) + "</p>"
+        );
+
+    QDialogButtonBox *buttons = new QDialogButtonBox(
+                QDialogButtonBox::Close, Qt::Horizontal, dialog.data() );
+    QObject::connect( buttons, SIGNAL(rejected()), dialog.data(), SLOT(close()) );
+
+    QPushButton *copyButton = new QPushButton( Notification::tr("&Copy"), buttons );
+    const QColor color = getDefaultIconColor(*copyButton, QPalette::Window);
+    const QIcon icon = iconFactory->getIcon("clipboard", IconPaste, color, color);
+    copyButton->setIcon(icon);
+    QObject::connect( copyButton, SIGNAL(clicked()), editor, SLOT(selectAll()) );
+    QObject::connect( copyButton, SIGNAL(clicked()), editor, SLOT(copy()) );
+    buttons->addButton(copyButton, QDialogButtonBox::ActionRole);
+
+    QVBoxLayout *layout = new QVBoxLayout( dialog.data() );
+    layout->addWidget(editor);
+    layout->addWidget(buttons);
+
+    dialog->adjustSize();
+    dialog->setAttribute(Qt::WA_DeleteOnClose);
+    dialog.take()->show();
+}
+
+} // namespace
 
 Notification::Notification(int id)
     : m_id(id)
@@ -138,18 +190,13 @@ void Notification::adjust()
 void Notification::mousePressEvent(QMouseEvent *event)
 {
     if (event->button() != Qt::LeftButton) {
-        QWidget::mousePressEvent(event);
+        if ( !m_textToCopy.isEmpty() )
+            showNotificationInspectDialog(m_titleLabel->text(), m_textToCopy);
         return;
     }
 
     if (m_timer != NULL)
         m_timer->stop();
-
-    if ( !m_textToCopy.isEmpty() ) {
-        QVariantMap dataMap;
-        setTextData(&dataMap, m_textToCopy);
-        QApplication::clipboard()->setMimeData( createMimeData(dataMap), QClipboard::Clipboard );
-    }
 
     emit closeNotification(this);
 }
