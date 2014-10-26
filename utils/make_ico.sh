aa142d8303937248ebee50f8c6a350855d8d228f
hluk <hluk@email.cz>
2014-10-26 07:42:20 -0700

    Update Windows icons

--- utils/make_ico.sh
+++ utils/make_ico.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-img1=src/images/icon.svg
+img1=src/images/icon.png
 img2=src/images/logo.png
 out=${1:-src/images/icon.ico}
 
 args=(
     -background transparent
     \( "$img1" -resize 16x16 \)
+    \( "$img1" -resize 20x20 \)
+    \( "$img1" -resize 24x24 \)
     -density 600
     \( "$img1" -resize 32x32 \)
+    \( "$img2" -resize 40x40 \)
     \( "$img2" -resize 48x48 \)
     \( "$img2" -resize 64x64 \)
     \( "$img2" -resize 96x96 \)
