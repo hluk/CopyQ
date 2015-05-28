ccb13a64b995a902dccbfc54f5997b8424393b42
Lukas Holecek <hluk@email.cz>
2015-05-28 21:03:20 +0200

    Tests: Increase wait time for window to show

--- src/tests/test_utils.h
+++ src/tests/test_utils.h
@@ -69,7 +69,7 @@ do { \
 } while(false)
 
 /// Interval to wait (in ms) until window is shown and focused.
-const int waitMsShow = 250;
+const int waitMsShow = 1000;
 
 /// Interval to wait (in ms) until search is complete.
 const int waitMsSearch = 250;
