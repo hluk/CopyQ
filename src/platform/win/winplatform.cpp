8b423ef733abe768f07b4e6490c011d38392c017
hluk <hluk@email.cz>
2016-02-13 22:13:51 -0800

    Windows: Fix displaying some shortcuts

--- src/platform/win/winplatform.cpp
+++ src/platform/win/winplatform.cpp
@@ -212,11 +212,26 @@ PlatformClipboardPtr WinPlatform::clipboard()
 
 int WinPlatform::keyCode(const QKeyEvent &event)
 {
+    const int key = PlatformNativeInterface::keyCode(event);
+
+    // Some keys shouldn't be translated.
+    if ( key == Qt::Key_Return
+      || key == Qt::Key_Enter
+      || key == Qt::Key_Escape
+      || key == Qt::Key_Tab
+      || key == Qt::Key_Backtab
+      || key == Qt::Key_Backspace
+         )
+    {
+        return key;
+    }
+
     const quint32 vk = event.nativeVirtualKey();
     const UINT result = MapVirtualKeyW(vk, MAPVK_VK_TO_CHAR);
     if (result != 0)
         return result;
-    return PlatformNativeInterface::keyCode(event);
+
+    return key;
 }
 
 QStringList WinPlatform::getCommandLineArguments(int, char**)
