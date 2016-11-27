8d2095d23b2986c4ed38f05327659420253421ba
Lukas Holecek <hluk@email.cz>
2016-11-27 17:40:24 +0100

    OSX: Fix building package

--- utils/build-osx.sh
+++ utils/build-osx.sh
@@ -8,6 +8,9 @@ mkdir -p build
 cd build
 
 /usr/local/opt/qt5/bin/qmake CONFIG+=release ..
-make dmg
+make
+make package_plugins
+make package_translations
+/usr/local/opt/qt5/bin/macdeployqt copyq.app -dmg
 
 cd "$root"
