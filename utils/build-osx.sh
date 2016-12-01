7f385f7a9ec0c4f9d8fc7b33ed1cb8eac7c9ee81
Lukas Holecek <hluk@email.cz>
2016-12-01 10:56:10 +0100

    OSX: Deploy package after fixing library paths

--- utils/build-osx.sh
+++ utils/build-osx.sh
@@ -7,22 +7,27 @@ root=$PWD
 mkdir -p build
 cd build
 
-/usr/local/opt/qt5/bin/qmake CONFIG+=release ..
+qt_bin="/usr/local/opt/qt5/bin"
+
+"$qt_bin/qmake" CONFIG+=release ..
 make
 make package_plugins
 make package_translations
 
-# Create CopyQ.dmg
-/usr/local/opt/qt5/bin/macdeployqt CopyQ.app -verbose=2 -dmg
+# Create "CopyQ.app/"
+"$qt_bin/macdeployqt" CopyQ.app -verbose=2 -always-overwrite
 
 executable="CopyQ.app/Contents/MacOS/copyq"
 
-# Fix dependencies
+# Fix paths for included Qt libraries.
 git clone https://github.com/iltommi/macdeployqtfix.git
 python macdeployqtfix/macdeployqtfix.py "$executable" /usr/local/Cellar/qt5
 
-# Test the deployed app
+# Test the app before deployment.
 "$executable" --help
 "$executable" --version
+
+# Create "CopyQ.dmg".
+"$qt_bin/macdeployqt" CopyQ.app -verbose=2 -dmg -no-plugins
 
 cd "$root"
