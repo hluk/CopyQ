b50b3b75ee63f16ab6c412f1341bc1aaa2e2a3e5
Lukas Holecek <hluk@email.cz>
2017-02-25 15:53:19 +0100

    AppVeyor: Test "info" argument

--- utils/appveyor/after_build.bat
+++ utils/appveyor/after_build.bat
@@ -32,6 +32,7 @@ xcopy /F "%BuildPlugins%\*.dll" "%Destination%\plugins" || goto :error
 
 "%Executable%" --help || goto :error
 "%Executable%" --version || goto :error
+"%Executable%" --info || goto :error
 "%Executable%" tests || goto :error
 
 7z a "%Name%.zip" -r "%Destination%" || goto :error
