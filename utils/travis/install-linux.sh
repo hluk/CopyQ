df3d0fbb5edcfa39d7283f9e4509b1d2b8aa3995
Lukas Holecek <hluk@email.cz>
2017-02-28 15:26:28 +0100

    Travis: Install gpg2

--- utils/travis/install-linux.sh
+++ utils/travis/install-linux.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-sudo apt-get install libqt4-dev libxtst-dev openbox
+sudo apt-get install libqt4-dev libxtst-dev openbox gnupg2
 
 # Coveralls
 pip install --user 'urllib3[secure]' cpp-coveralls
