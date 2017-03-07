83662f6ffb7867f9ce533a4191610a31abe96827
Lukas Holecek <hluk@email.cz>
2017-03-07 08:03:47 +0100

    Use Coverity from Travis CI

--- utils/appveyor/build_script.bat
+++
@@ -1,1 +0,0 @@
-cmake --build build/ --config Release
--- utils/travis/install-linux.sh
+++ utils/travis/install-linux.sh
@@ -3,6 +3,3 @@
 set -e -x
 
 sudo apt-get install libqt4-dev libxtst-dev openbox gnupg2
-
-# Coveralls
-pip install --user 'urllib3[secure]' cpp-coveralls
