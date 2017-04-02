7a4476cd2753edc10ba5f4eac3c9dee0e26c8b50
Lukas Holecek <hluk@email.cz>
2017-04-02 15:22:39 +0200

    OSX: Update icons

--- utils/make_icns.sh
+++ utils/make_icns.sh
@@ -1,47 +1,40 @@
 #!/bin/bash
+set -ex
 
 image_dir=src/images
 tmp_dir=icon.iconset
 
-img1=${image_dir}/icon.svg
-img2=${image_dir}/logo.svg
 out=${image_dir}/icon.icns
 
-if ! which rsvg-convert &>/dev/null
-then
-    echo "This script requires rsvg-convert"
-    echo "On OS X, you can install with"
-    echo " > brew install librsvg"
-    exit 1
-fi
-
-rm -rf ${tmp_dir}
-mkdir -p ${tmp_dir}
+rm -rf "${tmp_dir}"
+mkdir -p "${tmp_dir}"
 
 convert_img () {
-    img=$1
-    size=$2
-    size_name=$2
-    highdpi=$3
-    outfile=${tmp_dir}/icon_${size_name}x${size_name}
-    [[ -n "$highdpi" ]] \
-        && outfile="${outfile}@${highdpi}x" \
-        && size=$(expr $size \* $highdpi)
-    rsvg-convert -w $size -h $size -f png -o "${outfile}.png" "$img"
-    # ImageMagick doesn't get colors right..
-    # convert -background transparent $img -resize ${size}x${size} "${outfile}.png"
+    size=$1
+    highdpi=$2
+
+    target_img=${tmp_dir}/icon_${size}x${size}
+    if [[ -n "$highdpi" ]]; then
+        target_img="${target_img}@${highdpi}x"
+        size=$(expr $size \* $highdpi)
+    fi
+    target_img="${target_img}.png"
+
+    source_img=${image_dir}/icon_${size}x${size}.png
+
+    cp "$source_img" "$target_img"
 }
 
-convert_img $img1 16
-convert_img $img1 16 2
-convert_img $img2 32
-convert_img $img2 32 2
-convert_img $img2 128
-convert_img $img2 128 2
-convert_img $img2 256
-convert_img $img2 256 2
-convert_img $img2 512
-convert_img $img2 512 2
+convert_img 16
+convert_img 16 2
+convert_img 32
+convert_img 32 2
+convert_img 128
+convert_img 128 2
+convert_img 256
+convert_img 256 2
+convert_img 512
+convert_img 512 2
 
-iconutil --convert icns --output $out ${tmp_dir}
-rm -rf ${tmp_dir}
+iconutil --convert icns --output $out "${tmp_dir}"
+rm -rf "${tmp_dir}"
