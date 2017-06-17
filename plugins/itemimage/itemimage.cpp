9d239ee792640279d623b7f129ca744c059dd889
Lukas Holecek <hluk@email.cz>
2017-06-17 10:30:52 +0200

    itemimage: Use smooth thumbnails

--- plugins/itemimage/itemimage.cpp
+++ plugins/itemimage/itemimage.cpp
@@ -190,9 +190,9 @@ ItemWidget *ItemImageLoader::create(const QModelIndex &index, QWidget *parent, b
     const int w = preview ? 0 : m_settings.value("max_image_width", 320).toInt();
     const int h = preview ? 0 : m_settings.value("max_image_height", 240).toInt();
     if ( w > 0 && pix.width() > w && (h <= 0 || pix.width()/w > pix.height()/h) ) {
-        pix = pix.scaledToWidth(w);
+        pix = pix.scaledToWidth(w, Qt::SmoothTransformation);
     } else if (h > 0 && pix.height() > h) {
-        pix = pix.scaledToHeight(h);
+        pix = pix.scaledToHeight(h, Qt::SmoothTransformation);
     }
 
     QByteArray animationData;
