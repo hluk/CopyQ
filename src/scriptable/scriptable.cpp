dc3a1a019b1c84b04a0e632f49ec423e47a08164
Lukas Holecek <hluk@email.cz>
2017-11-17 15:15:50 +0100

    Fix handling client arguments serialization issue

--- src/scriptable/scriptable.cpp
+++ src/scriptable/scriptable.cpp
@@ -2093,7 +2093,9 @@ void Scriptable::executeArguments(const QByteArray &bytes)
     QDataStream stream(bytes);
     stream >> args;
     if ( stream.status() != QDataStream::Ok ) {
-        log("Failed to read client arguments", LogError);
+        const auto message = "Failed to read client arguments";
+        log(message, LogError);
+        emit sendMessage(message, CommandError);
         return;
     }
 
