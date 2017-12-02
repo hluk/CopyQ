56aba43c67c72bba3412f15a9d9825b1f1f3e133
Lukas Holecek <hluk@email.cz>
2017-12-02 13:03:06 +0100

    OSX: Fix build

--- utils/travis/install-osx.sh
+++ utils/travis/install-osx.sh
@@ -2,27 +2,4 @@
 
 set -e -x
 
-WHEELHOUSE="$HOME/.wheelhouse"
-
-function pip_with_cache() {
-    # Pre-cache all dependencies as wheels in the cache dir
-    pip wheel \
-        --use-wheel \
-        --wheel-dir="$WHEELHOUSE" \
-        --find-links="file://$WHEELHOUSE" \
-        "$@"
-
-    # Install from the wheelhouse
-    sudo pip install \
-        --use-wheel \
-        --find-links="file://$WHEELHOUSE" \
-        "$@"
-}
-
 brew install qt5
-sudo pip install --upgrade pip
-sudo pip install wheel
-pip_with_cache \
-    dmgbuild \
-    'pyobjc-framework-Quartz' \
-    'pyobjc-framework-Cocoa'
