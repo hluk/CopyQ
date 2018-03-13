2aa4616ad8ca248f1a22f6d64db1a529c696d74d
Martin Lepadusch <mlepadusch@googlemail.com>
2018-03-13 20:07:11 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (1258 of 1258 strings)

--- translations/copyq_de.ts
+++ translations/copyq_de.ts
@@ -2445,12 +2445,12 @@ Use Item Preview to display whole items.</source>
     <message>
         <location filename="../src/ui/configurationmanager.ui" line="768"/>
         <source>Enable searching for numbers, otherwise pressing a digit key activates item on that position</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Suchen nach Zahlen ermöglichen. Falls diese Option deaktiviert ist, dann wird durch die Eingabe einer Zahl das Element an dieser Position aktiviert</translation>
     </message>
     <message>
         <location filename="../src/ui/configurationmanager.ui" line="771"/>
         <source>S&amp;earch for numbers</source>
-        <translation type="unfinished"></translation>
+        <translation>Such&amp;en nach Zahlen</translation>
     </message>
     <message>
         <location filename="../src/ui/configurationmanager.ui" line="784"/>
