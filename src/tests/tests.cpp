8f2767dc553bcd82c7e9ff182d0219a6c9f67cb2
Lukas Holecek <hluk@email.cz>
2019-03-01 11:25:27 +0100

    Tests: Fix clipboard dialog content test
    Signed-off-by: Lukas Holecek <hluk@email.cz>

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -2974,15 +2974,16 @@ void Tests::showHideAboutDialog()
 
 void Tests::showHideClipboardDialog()
 {
-    TEST( m_test->setClipboard("TEST", "text/test-format") );
+    TEST( m_test->setClipboard("TEST", "test-format") );
     RUN("keys" << clipboardBrowserId << "CTRL+SHIFT+C" << clipboardDialogId, "");
 
-    RUN("keys" << clipboardDialogId << "DOWN" << clipboardDialogFormatListId, "");
+    RUN("keys" << clipboardDialogId << "DOWN" << "HOME" << clipboardDialogFormatListId, "");
+
     RUN("keys" << clipboardDialogFormatListId << keyNameFor(QKeySequence::Copy), "");
 #ifdef Q_OS_WIN
-    WAIT_FOR_CLIPBOARD("application/x-qt-windows-mime;value=\"text/test-format\"");
+    WAIT_FOR_CLIPBOARD("application/x-qt-windows-mime;value=\"test-format\"");
 #else
-    WAIT_FOR_CLIPBOARD("text/test-format");
+    WAIT_FOR_CLIPBOARD("test-format");
 #endif
 
     RUN("keys" << clipboardDialogId << "ESCAPE" << clipboardBrowserId, "");
