86ffa4d0240c451653595fc719c0946168ae744e
Andrew Novikov <as.asaw@gmail.com>
2019-03-25 22:07:00 +0300

    Update script-osx.sh

--- utils/travis/script-osx.sh
+++ utils/travis/script-osx.sh
@@ -3,33 +3,20 @@
 
 set -exuo pipefail
 
-version=$(git describe --match='v*')
-
 root=$PWD
 mkdir -p build
 cd build
 
-qt_bin="/usr/local/opt/qt5/bin"
-
-"$qt_bin/qmake" \
-    CONFIG+=release \
-    CONFIG+=tests \
-    DEFINES+=COPYQ_VERSION=\\\\\\\"\"$version\\\\\\\"\" \
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_TESTS=ON \
     ..
 
-make
-make package_plugins
-make package_translations
-make package_themes
+cmake --build . --target all --parallel
 
-# Create "CopyQ.app/"
-"$qt_bin/macdeployqt" CopyQ.app -verbose=2 -always-overwrite
+cpack
 
-executable="CopyQ.app/Contents/MacOS/copyq"
-
-# Fix paths for included Qt libraries.
-git clone https://github.com/iltommi/macdeployqtfix.git
-python macdeployqtfix/macdeployqtfix.py "$executable" /usr/local/Cellar/qt5
+executable="$(ls ${PWD}/_CPack_Packages/Darwin/DragNDrop/copyq-*/CopyQ.app/Contents/MacOS/CopyQ)"
 
 # Test the app before deployment.
 "$executable" --help
@@ -49,6 +36,6 @@ export COPYQ_TESTS_SKIP_CONFIG_MOVE=1
     "$executable" tests
 
 # Create "CopyQ.dmg".
-"$qt_bin/macdeployqt" CopyQ.app -verbose=2 -dmg -no-plugins
+cp -a copyq-*.dmg CopyQ.dmg
 
 cd "$root"
