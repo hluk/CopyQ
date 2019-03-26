b204c5c138a4e962cfb4fbe2353172a498ef632f
Andrew Novikov <as.asaw@gmail.com>
2019-03-26 15:40:48 +0300

    Change the order of commands

--- utils/travis/script-osx.sh
+++ utils/travis/script-osx.sh
@@ -39,10 +39,10 @@ export COPYQ_TESTS_SKIP_CONFIG_MOVE=1
 "$executable" tests ||
     "$executable" tests
 
-# Create "CopyQ.dmg".
-cp -a copyq-*.dmg CopyQ.dmg
-
 # Print dependencies to let us further make sure that we don't depend on local libraries
 otool -L $executable
 otool -L ${app_bundle_path}/Contents/PlugIns/copyq/*
 otool -L ${app_bundle_path}/Contents/Frameworks/Qt*.framework/Versions/5/Qt*
+
+# Create "CopyQ.dmg".
+cp -a copyq-*.dmg CopyQ.dmg
