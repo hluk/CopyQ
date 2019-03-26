8a87fe2701ae7c0d973719906318b5624785f606
Andrew Novikov <as.asaw@gmail.com>
2019-03-26 20:48:36 +0300

    Build an App Bundle on macOS with CMake and CPack (#1113)
    * Build an App Bundle on macOS with CMake and CPack
    
    * Update script-osx.sh
    
    * An attempt to fix the macOS build
    
    * Don't use --parallel
    
    * Implement additional checks for dependencies
    
    * Comments
    
    * Change the order of commands

--- utils/travis/script-osx.sh
+++ utils/travis/script-osx.sh
@@ -3,33 +3,21 @@
 
 set -exuo pipefail
 
-version=$(git describe --match='v*')
-
-root=$PWD
 mkdir -p build
 cd build
 
-qt_bin="/usr/local/opt/qt5/bin"
-
-"$qt_bin/qmake" \
-    CONFIG+=release \
-    CONFIG+=tests \
-    DEFINES+=COPYQ_VERSION=\\\\\\\"\"$version\\\\\\\"\" \
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH="$(brew --prefix qt5)" \
+    -DWITH_TESTS=ON \
     ..
 
-make
-make package_plugins
-make package_translations
-make package_themes
+cmake --build . --target all
 
-# Create "CopyQ.app/"
-"$qt_bin/macdeployqt" CopyQ.app -verbose=2 -always-overwrite
+cpack
 
-executable="CopyQ.app/Contents/MacOS/copyq"
-
-# Fix paths for included Qt libraries.
-git clone https://github.com/iltommi/macdeployqtfix.git
-python macdeployqtfix/macdeployqtfix.py "$executable" /usr/local/Cellar/qt5
+app_bundle_path="_CPack_Packages/Darwin/DragNDrop/copyq-*/CopyQ.app"
+executable="$(ls ${PWD}/${app_bundle_path}/Contents/MacOS/CopyQ)"
 
 # Test the app before deployment.
 "$executable" --help
@@ -42,13 +30,19 @@ ls "$("$executable" info themes)/"
 ls "$("$executable" info translations)/"
 test "$("$executable" info has-global-shortcuts)" -eq "1"
 
+# Uninstall local Qt to make sure we only use libraries from the bundle
+brew uninstall --force qt5
+
 # Run tests (retry once on error).
 export COPYQ_TESTS_SKIP_COMMAND_EDIT=1
 export COPYQ_TESTS_SKIP_CONFIG_MOVE=1
 "$executable" tests ||
     "$executable" tests
 
-# Create "CopyQ.dmg".
-"$qt_bin/macdeployqt" CopyQ.app -verbose=2 -dmg -no-plugins
+# Print dependencies to let us further make sure that we don't depend on local libraries
+otool -L $executable
+otool -L ${app_bundle_path}/Contents/PlugIns/copyq/*
+otool -L ${app_bundle_path}/Contents/Frameworks/Qt*.framework/Versions/5/Qt*
 
-cd "$root"
+# Create "CopyQ.dmg".
+cp -a copyq-*.dmg CopyQ.dmg
