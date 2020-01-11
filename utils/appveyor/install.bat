5b6409fb0170fedd12fc2588e6a115204371044f
Lukas Holecek <hluk@email.cz>
2020-01-11 09:46:35 +0100

    Windows: Drop MSVC builds (broken paths in gpg)
    Signed-off-by: Lukas Holecek <hluk@email.cz>

--- utils/appveyor/install.bat
+++ utils/appveyor/install.bat
@@ -7,9 +7,7 @@ dir %QTDIR%\bin || goto :error
 dir %QTDIR%\lib\cmake || goto :error
 
 REM Note: Following removes sh.exe from PATH so that CMake can generate MinGW Makefile.
-if DEFINED MINGW_PATH set PATH=%MINGW_PATH%\bin;%PATH:C:\Program Files\Git\usr\bin;=%
-
-if DEFINED CMD_VCVARSALL call %CMD_VCVARSALL%
+PATH=%MINGW_PATH%\bin;%PATH:C:\Program Files\Git\usr\bin;=%
 
 :error
 exit /b %errorlevel%
