a531fb3ad900b1722a660b61db0ef9425b6e05a7
Lukas Holecek <hluk@email.cz>
2020-04-13 07:32:14 +0200

    Update command line help
    Signed-off-by: Lukas Holecek <hluk@email.cz>

--- src/scriptable/commandhelp.cpp
+++ src/scriptable/commandhelp.cpp
@@ -22,6 +22,8 @@
 #include "common/common.h"
 #include "scriptable/scriptable.h"
 
+constexpr int helpTextColumn = 23;
+
 CommandHelp::CommandHelp()
     : cmd()
     , desc()
@@ -48,12 +50,18 @@ QString CommandHelp::toString() const
     if (cmd.isNull())
         return "\n";
 
-    const int indent = 23;
-    bool indentFirst = desc.startsWith('\n');
-    return QString("    %1").arg(cmd + args, indentFirst ? 0 : -indent)
-            + (indentFirst ? QString() : QString("  "))
-            + QString(desc).replace('\n', "\n" + QString(' ')
-                                    .repeated(4 + 2 + (indentFirst ? 0 : indent))) + "\n";
+    const auto cmdArgs = cmd + args;
+    if ( cmdArgs.size() > helpTextColumn - 2 || desc.contains('\n') ) {
+        const auto indent = QString(' ').repeated(6);
+        return QString("    %1\n%2").arg(cmdArgs, indent)
+                + QString(desc)
+                    .replace('\n', "\n" + indent) + "\n";
+    }
+
+    return QString("    %1 ").arg(cmdArgs, -helpTextColumn)
+            + QString(desc)
+                .replace('\n', "\n" + QString(' ')
+                .repeated(6 + helpTextColumn)) + "\n";
 }
 
 QList<CommandHelp> commandHelp()
@@ -84,9 +92,12 @@ QList<CommandHelp> commandHelp()
             << CommandHelp("paste",
                            Scriptable::tr("Paste clipboard to current window\n"
                                           "(may not work with some applications)."))
+            << CommandHelp("copy",
+                           Scriptable::tr("Copy clipboard from current window\n"
+                                          "(may not work with some applications)."))
             << CommandHelp("copy", Scriptable::tr("Set clipboard text."))
                .addArg(Scriptable::tr("TEXT"))
-            << CommandHelp("copy", Scriptable::tr("\nSet clipboard content."))
+            << CommandHelp("copy", Scriptable::tr("Set clipboard content."))
                .addArg(Scriptable::tr("MIME"))
                .addArg(Scriptable::tr("DATA"))
                .addArg("[" + Scriptable::tr("MIME") + " " + Scriptable::tr("DATA") + "]...")
@@ -121,7 +132,7 @@ QList<CommandHelp> commandHelp()
             << CommandHelp("read",
                            Scriptable::tr("Print raw data of clipboard or item in row."))
                .addArg("[" + Scriptable::tr("MIME") + "|" + Scriptable::tr("ROW") + "]...")
-            << CommandHelp("write", Scriptable::tr("\nWrite raw data to given row."))
+            << CommandHelp("write", Scriptable::tr("Write raw data to given row."))
                .addArg("[" + Scriptable::tr("ROW") + "=0]")
                .addArg(Scriptable::tr("MIME"))
                .addArg(Scriptable::tr("DATA"))
@@ -131,12 +142,12 @@ QList<CommandHelp> commandHelp()
                            Scriptable::tr("Show action dialog."))
                .addArg("[" + Scriptable::tr("ROWS") + "=0...]")
             << CommandHelp("action",
-                           Scriptable::tr("\nRun PROGRAM on item text in the rows.\n"
+                           Scriptable::tr("Run PROGRAM on item text in the rows.\n"
                                           "Use %1 in PROGRAM to pass text as argument."))
                .addArg("[" + Scriptable::tr("ROWS") + "=0...]")
                .addArg("[" + Scriptable::tr("PROGRAM") + " [" + Scriptable::tr("SEPARATOR") + "=\\n]]")
             << CommandHelp("popup",
-                           Scriptable::tr("\nShow tray popup message for TIME milliseconds."))
+                           Scriptable::tr("Show tray popup message for TIME milliseconds."))
                .addArg(Scriptable::tr("TITLE"))
                .addArg(Scriptable::tr("MESSAGE"))
                .addArg("[" + Scriptable::tr("TIME") + "=8000]")
@@ -174,19 +185,17 @@ QList<CommandHelp> commandHelp()
                .addArg(Scriptable::tr("OPTION"))
                .addArg(Scriptable::tr("VALUE"))
             << CommandHelp()
-            << CommandHelp("eval, -e",
-                           Scriptable::tr("\nEvaluate ECMAScript program.\n"
-                                          "Arguments are accessible using with \"arguments[0..N]\"."))
+            << CommandHelp("eval, -e", Scriptable::tr("Evaluate script."))
                .addArg("[" + Scriptable::tr("SCRIPT") + "]")
                .addArg("[" + Scriptable::tr("ARGUMENTS") + "]...")
             << CommandHelp("session, -s, --session",
-                           Scriptable::tr("\nStarts or connects to application instance with given session name."))
+                           Scriptable::tr("Starts or connects to application instance with given session name."))
                .addArg(Scriptable::tr("SESSION"))
             << CommandHelp("help, -h, --help",
-                           Scriptable::tr("\nPrint help for COMMAND or all commands."))
+                           Scriptable::tr("Print help for COMMAND or all commands."))
                .addArg("[" + Scriptable::tr("COMMAND") + "]...")
             << CommandHelp("version, -v, --version",
-                           Scriptable::tr("\nPrint version of program and libraries."))
+                           Scriptable::tr("Print version of program and libraries."))
 #ifdef HAS_TESTS
             << CommandHelp("tests, --tests",
                            Scriptable::tr("Run application tests (append --help argument for more info)."))
