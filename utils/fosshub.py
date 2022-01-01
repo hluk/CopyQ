4769fc45c88dcea434b04cfd0eaf15e34b77b975
Lukas Holecek <hluk@email.cz>
2022-01-01 16:42:48 +0100

    Update fosshub script

--- utils/fosshub.py
+++ utils/fosshub.py
@@ -13,8 +13,8 @@ project_id = '5c1195728c9fe8186f80a14b'
 fosshub_new_release_url = 'https://api.fosshub.com/rest/projects/{project_id}/releases/'.format(project_id=project_id)
 github_release_url = 'https://github.com/hluk/CopyQ/releases/download/v{version}/{basename}'
 files = {
-    'copyq-v{version}-setup.exe': 'Windows Installer',
-    'copyq-v{version}.zip': 'Windows Portable',
+    'copyq-{version}-setup.exe': 'Windows Installer',
+    'copyq-{version}.zip': 'Windows Portable',
     'CopyQ.dmg.zip': 'macOS',
 }
 
