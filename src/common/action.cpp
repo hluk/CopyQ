7299e421c0677a161a424aee55dc52d83351e702
Lukas Holecek <hluk@email.cz>
2022-02-06 09:38:19 +0100

    Fix using Windows path separators in editor commands
    Avoids treating Window path separators as escape characters in commands
    that start with drive letter (for example "C:\").
    
    Fixes #1894 #1868

--- src/common/action.cpp
+++ src/common/action.cpp
@@ -29,6 +29,7 @@
 #include <QEventLoop>
 #include <QPointer>
 #include <QProcessEnvironment>
+#include <QRegularExpression>
 #include <QTimer>
 
 namespace {
@@ -81,6 +82,10 @@ QList< QList<QStringList> > parseCommands(const QString &cmd, const QStringList 
     bool escape = false;
     bool percent = false;
 
+    // Ignore escape sequences if command starts with an unescaped Windows path.
+    const QRegularExpression reUnescapedWindowsPath(R"(^[a-zA-Z]:\\[^\\])");
+    const bool allowEscape = !cmd.contains(reUnescapedWindowsPath);
+
     for (int i = 0; i < cmd.size(); ++i) {
         const QChar &c = cmd[i];
 
@@ -104,7 +109,7 @@ QList< QList<QStringList> > parseCommands(const QString &cmd, const QStringList 
             } else {
                 arg.append(c);
             }
-        } else if (c == '\\') {
+        } else if (allowEscape && c == '\\') {
             escape = true;
         } else if (!quote.isNull()) {
             if (quote == c) {
