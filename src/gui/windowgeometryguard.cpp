07745637b9d9ea8fefc6d6642163326f3c2ca23e
Lukas Holecek <hluk@email.cz>
2022-03-29 17:20:47 +0200

    Fix setting window geometry in quick succession

--- src/gui/windowgeometryguard.cpp
+++ src/gui/windowgeometryguard.cpp
@@ -191,7 +191,7 @@ void WindowGeometryGuard::onScreenChanged()
     }
 
     setGeometryGuardBlockedUntilHidden(m_window, true);
-    lockWindowGeometry();
+    m_timerUnlockGeometry.start();
     if (isMousePositionSupported || m_window->isModal()) {
         ::restoreWindowGeometry(m_window, true);
     } else {
