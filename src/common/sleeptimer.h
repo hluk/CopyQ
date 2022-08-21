ae1b10fcaf142abb97655ef95a5f88014fb8d433
Lukas Holecek <hluk@email.cz>
2022-08-21 07:15:31 +0200

    Fix compiler warnings
    Fixes following gcc compiler warnings:
    
        warning: assuming signed overflow does not occur when changing X +-
        C1 cmp C2 to X cmp C2 -+ C1 [-Wstrict-overflow]

--- src/common/sleeptimer.h
+++ src/common/sleeptimer.h
@@ -35,9 +35,10 @@ public:
 
     bool sleep()
     {
-        if (--m_minSleepCount < 0 && m_timer.elapsed() >= m_timeoutMs)
+        if (m_minSleepCount <= 0 && m_timer.elapsed() >= m_timeoutMs)
             return false;
 
+        --m_minSleepCount;
         QCoreApplication::processEvents(QEventLoop::AllEvents, 5);
         return true;
     }
