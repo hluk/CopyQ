634890ee5f67c093af2561f0bfbf615935e885ab
Lukas Holecek <hluk@email.cz>
2023-04-08 13:50:14 +0200

    Qt6: Fix closing the app if window is open

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -716,8 +716,14 @@ void MainWindow::exit()
 
 void MainWindow::closeEvent(QCloseEvent *event)
 {
-    event->ignore();
     hideWindow();
+#if QT_VERSION >= QT_VERSION_CHECK(5,15,0)
+    event->accept();
+#else
+    // This is needed in older versions of Qt (tested in Qt 5.12.8),
+    // otherwise focusing is somewhat broken (test pasteFromMainWindow()).
+    event->ignore();
+#endif
     COPYQ_LOG("Got main window close event.");
 }
 
