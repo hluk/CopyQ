d1b4f28158d17a77fcc036df84f3e6e7fb691643
Lukas Holecek <hluk@email.cz>
2024-04-18 17:46:35 +0200

    Windows: Detect secrets in clipboard using more formats
    Fixes #2679

--- src/platform/win/winplatformclipboard.cpp
+++ src/platform/win/winplatformclipboard.cpp
@@ -6,6 +6,15 @@
 #include <QMimeData>
 #include <QTimer>
 
+namespace {
+
+bool contains(const QStringList &formats, const QMimeData &data, const QString &format, const QByteArray &value)
+{
+    return formats.contains(format) && data.data(format) == value;
+}
+
+} // namespace
+
 void WinPlatformClipboard::startMonitoring(const QStringList &formats)
 {
     m_lastClipboardSequenceNumber = GetClipboardSequenceNumber();
@@ -25,9 +34,25 @@ void WinPlatformClipboard::startMonitoring(const QStringList &formats)
 
 bool WinPlatformClipboard::isHidden(const QMimeData &data) const
 {
+    static const QByteArray zeroDWord(4, '\0');
+
     // QMimeData::hasFormat does not seem to work in this case (in Qt 5.15.2).
-    return data.formats().contains(
-        QStringLiteral("application/x-qt-windows-mime;value=\"Clipboard Viewer Ignore\""));
+    const QStringList formats = data.formats();
+
+    return formats.contains(
+            QLatin1String("application/x-qt-windows-mime;value=\"Clipboard Viewer Ignore\""))
+
+        // https://learn.microsoft.com/en-us/windows/win32/dataxchg/clipboard-formats#cloud-clipboard-and-clipboard-history-formats
+        || formats.contains(
+            QLatin1String("application/x-qt-windows-mime;value=\"ExcludeClipboardContentFromMonitorProcessing\""))
+        || contains(formats, data,
+            QStringLiteral("application/x-qt-windows-mime;value=\"CanIncludeInClipboardHistory\""), zeroDWord)
+        || contains(formats, data,
+            QStringLiteral("application/x-qt-windows-mime;value=\"CanUploadToCloudClipboard\""), zeroDWord)
+
+        // This is set by some Qt apps
+        || contains(formats, data,
+            QStringLiteral("ExcludeClipboardContentFromMonitorProcessing"), QByteArrayLiteral("1"));
 }
 
 void WinPlatformClipboard::onChanged(int)
