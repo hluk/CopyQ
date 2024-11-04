8afa788459b23469d7ca3a7dbc07eb3e7a23e4ba
Lukas Holecek <hluk@email.cz>
2024-11-04 14:12:11 +0100

    Fix building with Qt 5

--- src/common/navigationstyle.h
+++ src/common/navigationstyle.h
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #pragma once
 
+#include <QtGlobal>
+#if QT_VERSION < QT_VERSION_CHECK(6,0,0)
+#   include <QMetaType>
+#endif
+
 enum class NavigationStyle {
     Default,
     Vi,
     Emacs
 };
+
+#if QT_VERSION < QT_VERSION_CHECK(6,0,0)
+Q_DECLARE_METATYPE(NavigationStyle)
+#endif
