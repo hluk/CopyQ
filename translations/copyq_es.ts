0eab85aa26b9461e342fadc8b40de9e8f97f5481
gallegonovato <fran-carro@hotmail.es>
2024-11-09 09:46:28 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (638 of 638 strings)

--- translations/copyq_es.ts
+++ translations/copyq_es.ts
@@ -764,7 +764,16 @@ Examples:
 - Match PDF filenames: \.pdf$
 - Match single character: ^.$
 - Match remote multimedia: ^http://.*\.(ogv|vlc|mp4|mp3)$</source>
-        <translation type="unfinished"></translation>
+        <translation>Omite el comando si el texto de entrada no coincide con esta expresión regular (déjela vacía para que coincida con todo).
+
+%2 a %9 (o argumento[1] en adelante en el script) en Comando y Filtro serán reemplazados por los textos capturados.
+
+Ejemplos:
+
+- URL coincidente: ^(https?|ftp)://
+- Coincidir con nombres de archivos PDF: \.pdf$
+- Coincidencia de un solo carácter: ^.$
+- Coincidir con multimedia remoto: ^http://.*\.(ogv|vlc|mp4|mp3)$</translation>
     </message>
     <message>
         <location filename="../src/ui/commandwidget.ui" line="389"/>
@@ -1105,27 +1114,27 @@ pulsar F2 para editar.</translation>
     <message>
         <location filename="../src/ui/configtabgeneral.ui" line="161"/>
         <source>Navigation style / Keymap:</source>
-        <translation type="unfinished"></translation>
+        <translation>Estilo de navegación / Mapa de teclas:</translation>
     </message>
     <message>
         <location filename="../src/ui/configtabgeneral.ui" line="171"/>
         <source>Support for Vi navigation (keys H, J, K, L, / and more) and Emacs navigation (Ctrl+N, P, V and more)</source>
-        <translation type="unfinished"></translation>
+        <translation>Compatibilidad con navegación Vi (teclas H, J, K, L, / y más) y navegación Emacs (Ctrl+N, P, V y más)</translation>
     </message>
     <message>
         <location filename="../src/ui/configtabgeneral.ui" line="175"/>
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Por defecto</translation>
     </message>
     <message>
         <location filename="../src/ui/configtabgeneral.ui" line="180"/>
         <source>Vi</source>
-        <translation type="unfinished"></translation>
+        <translation>Vi</translation>
     </message>
     <message>
         <location filename="../src/ui/configtabgeneral.ui" line="185"/>
         <source>Emacs</source>
-        <translation type="unfinished"></translation>
+        <translation>Emacs</translation>
     </message>
     <message>
         <location filename="../src/ui/configtabgeneral.ui" line="208"/>
