078fdcc5acaf1036fdb4a108f262230d3886c217
Lukas Holecek <hluk@email.cz>
2025-10-11 11:32:37 +0200

    Drop redundant Qt log level classifier

--- src/tests/tests_common.cpp
+++ src/tests/tests_common.cpp
@@ -41,55 +41,55 @@ bool testStderr(const QByteArray &stderrData, TestInterface::ReadStderrFlag flag
         plain("Warning: CopyQ server is already running"),
 
         // X11 (Linux)
-        plain("QtWarning: QXcbXSettings::QXcbXSettings(QXcbScreen*) Failed to get selection owner for XSETTINGS_S atom"),
-        plain("QtWarning: QXcbConnection: XCB error:"),
-        plain("QtWarning: QXcbClipboard: SelectionRequest too old"),
-        plain("QtWarning: libpng warning: iCCP: known incorrect sRGB profile"),
-        plain("QtWarning: QMime::convertToMime: unhandled mimetype: text/plain"),
-        plain("[kf.notifications] QtWarning: Failed to notify \"Created too many similar notifications in quick succession\""),
+        plain("QXcbXSettings::QXcbXSettings(QXcbScreen*) Failed to get selection owner for XSETTINGS_S atom"),
+        plain("QXcbConnection: XCB error:"),
+        plain("QXcbClipboard: SelectionRequest too old"),
+        plain("libpng warning: iCCP: known incorrect sRGB profile"),
+        plain("QMime::convertToMime: unhandled mimetype: text/plain"),
+        plain("[kf.notifications] Failed to notify \"Created too many similar notifications in quick succession\""),
 
         // Wayland (Linux)
-        plain("QtWarning: Wayland does not support QWindow::requestActivate()"),
-        plain("QtWarning: Unexpected wl_keyboard.enter event"),
-        plain("QtWarning: The compositor sent a wl_pointer.enter"),
-        plain("QtWarning: QObject::connect: No such signal QPlatformNativeInterface::systemTrayWindowChanged(QScreen*)"),
+        plain("Wayland does not support QWindow::requestActivate()"),
+        plain("Unexpected wl_keyboard.enter event"),
+        plain("The compositor sent a wl_pointer.enter"),
+        plain("QObject::connect: No such signal QPlatformNativeInterface::systemTrayWindowChanged(QScreen*)"),
 
         // Windows
-        plain("QtWarning: QWindowsWindow::setGeometry: Unable to set geometry"),
-        plain("QtWarning: QWinEventNotifier: no event dispatcher, application shutting down? Cannot deliver event."),
-        plain("QtWarning: setGeometry: Unable to set geometry"),
+        plain("QWindowsWindow::setGeometry: Unable to set geometry"),
+        plain("QWinEventNotifier: no event dispatcher, application shutting down? Cannot deliver event."),
+        plain("setGeometry: Unable to set geometry"),
 
-        plain("ERROR: QtCritical: QWindowsPipeWriter::write failed. (The pipe is being closed.)"),
-        plain("ERROR: QtCritical: QWindowsPipeWriter: asynchronous write failed. (The pipe has been ended.)"),
+        plain("ERROR: QWindowsPipeWriter::write failed. (The pipe is being closed.)"),
+        plain("ERROR: QWindowsPipeWriter: asynchronous write failed. (The pipe has been ended.)"),
 
-        plain("[kf.notifications] QtWarning: Received a response for an unknown notification."),
+        plain("[kf.notifications] Received a response for an unknown notification."),
         // KStatusNotifierItem
-        plain("[kf.windowsystem] QtWarning: Could not find any platform plugin"),
+        plain("[kf.windowsystem] Could not find any platform plugin"),
 
-        regex("QtWarning: QTemporaryDir: Unable to remove .* most likely due to the presence of read-only files."),
+        regex("QTemporaryDir: Unable to remove .* most likely due to the presence of read-only files."),
 
         // Windows Qt 5.15.2
-        plain("[qt.qpa.mime] QtWarning: Retrying to obtain clipboard."),
-        plain("[default] QtWarning: QSystemTrayIcon::setVisible: No Icon set"),
+        plain("[qt.qpa.mime] Retrying to obtain clipboard."),
+        plain("[default] QSystemTrayIcon::setVisible: No Icon set"),
 
         // macOS
-        plain("QtWarning: Failed to get QCocoaScreen for NSObject(0x0)"),
+        plain("Failed to get QCocoaScreen for NSObject(0x0)"),
         plain("ERROR: Failed to open session mutex: QSystemSemaphore::handle:: ftok failed"),
-        plain("Warning: [qt.tlsbackend.ossl] QtWarning: Failed to load libssl/libcrypto."),
-        regex(R"(QtWarning: Window position.* outside any known screen.*)"),
-        regex(R"(QtWarning: Populating font family aliases took .* ms. Replace uses of missing font family "Font Awesome.*" with one that exists to avoid this cost.)"),
+        plain("Warning: [qt.tlsbackend.ossl] Failed to load libssl/libcrypto."),
+        regex(R"(Window position.* outside any known screen.*)"),
+        regex(R"(Populating font family aliases took .* ms. Replace uses of missing font family "Font Awesome.*" with one that exists to avoid this cost.)"),
 
         // New in Qt 5.15.0
-        regex(R"(QtWarning: Populating font family aliases took .* ms. Replace uses of missing font family "Monospace" with one that exists to avoid this cost.)"),
+        regex(R"(Populating font family aliases took .* ms. Replace uses of missing font family "Monospace" with one that exists to avoid this cost.)"),
 
         // New in Qt 6.5
-        regex("QtWarning: Error in contacting registry"),
+        regex("Error in contacting registry"),
 
         // KNotification bug
-        plain(R"(QtWarning: QLayout: Attempting to add QLayout "" to QWidget "", which already has a layout)"),
+        plain(R"(QLayout: Attempting to add QLayout "" to QWidget "", which already has a layout)"),
 
         // Warnings from itemsync plugin, not sure what it causes
-        regex(R"(QtWarning: Could not remove our own lock file .* maybe permissions changed meanwhile)"),
+        regex(R"(Could not remove our own lock file .* maybe permissions changed meanwhile)"),
     };
     static QHash<QString, bool> ignoreLog;
 
