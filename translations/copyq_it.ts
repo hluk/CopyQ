19eaf835b8afc7007ee2de4f2da18e7f66171f6e
bovirus <roberto.boriotti@canon.it>
2025-11-14 08:07:41 +0100

    Translated using Weblate (Italian)
    Currently translated at 100.0% (641 of 641 strings)

--- translations/copyq_it.ts
+++ translations/copyq_it.ts
@@ -506,12 +506,12 @@ Puoi impostare il comando nelle preferenze.</translation>
     <message>
         <location filename="../src/gui/commanddialog.cpp" line="277"/>
         <source>Save Commands</source>
-        <translation type="unfinished"></translation>
+        <translation>Salva comandi</translation>
     </message>
     <message>
         <location filename="../src/gui/commanddialog.cpp" line="278"/>
         <source>Failed to save commands to file &quot;%1&quot;: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Impossibile salvare i comandi nel file &quot;%1&quot;: %2</translation>
     </message>
 </context>
 <context>
