87e95bb7bd183f9c6df42f670458436da7e00394
Pavlo <acid@jack.kyiv.ua>
2025-11-15 07:33:17 +0100

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (641 of 641 strings)

--- translations/copyq_uk.ts
+++ translations/copyq_uk.ts
@@ -506,12 +506,12 @@ You can set up the command in preferences.</source>
     <message>
         <location filename="../src/gui/commanddialog.cpp" line="277"/>
         <source>Save Commands</source>
-        <translation type="unfinished"></translation>
+        <translation>Зберегти команди</translation>
     </message>
     <message>
         <location filename="../src/gui/commanddialog.cpp" line="278"/>
         <source>Failed to save commands to file &quot;%1&quot;: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалось зберегти команди в файл &quot;%1&quot;: %2</translation>
     </message>
 </context>
 <context>
